666e6e05898637d36db2194ac6b82ad7fcddf892
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-10-14 21:58:13 +0200

    Exercise 3.1.

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+let phonebook = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+      },
+      { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+      },
+      { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Go to the proper <a href="http://localhost:3001/api/persons">page</a></h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
