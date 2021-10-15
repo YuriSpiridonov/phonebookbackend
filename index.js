7b2403f9f67030839f94ca79f1bfb2e6167519c7
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-10-15 18:31:24 +0200

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -25,11 +25,28 @@ let phonebook = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Go to the proper <a href="http://localhost:3001/api/persons">page</a></h1>')
+    response.send(
+        `<h1>
+            Go to the proper 
+            <a href="http://localhost:3001/api/persons">page</a>
+            or check 
+            <a href="http://localhost:3001/info">info</a>.
+        </h1>`)
 })
 
 app.get('/api/persons', (request, response) => {
     response.json(phonebook)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebook.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request, response) => {
@@ -40,9 +57,8 @@ app.get('/info', (request, response) => {
             <p>Phonebook has info for ${phonebook.length} people</p>
         </div>
         <div>
-        <p>${currentDate}</p>
-        </div>
-        `
+            <p>${currentDate}</p>
+        </div>`
     )
 })
 
